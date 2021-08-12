84bbaf7b31050adf861e01d74708abd52ab8ecf7
c3bull <kubcor1@gmail.com>
2021-08-12 13:46:40 +0200

    problem z wyswietlaniem wszystkich postów

--- chirper/src/components/mainPage.jsx
+++ chirper/src/components/mainPage.jsx
@@ -5,12 +5,16 @@ import axios from "axios";
 import {Badge, Box, ChakraProvider, Image} from "@chakra-ui/react";
 
 const MainPage = () => {
+
+
     const [count, setCount] = useState(0);
-    const [posts, setPosts] = useState(5);
+    const [posts, setPosts] = useState(null);
+    let posts2;
+
 
     useEffect(() => {
         // event.preventDefault();
-        setCount(1);
+        setCount(0);
         axios({
             method: 'post',
             url: 'http://localhost:3001/api/feed/posts',
@@ -18,72 +22,83 @@ const MainPage = () => {
                 author: '1234@test.pl'
             }
         }).then((response) => {
+
+            console.log('response. data ' + JSON.stringify(response.data))
+
             setPosts(response.data);
+            console.log('posts2  ' + JSON.stringify(posts))
         }).catch((error) => {
             console.log(error);
         });
     }, [count]);
 
 
-
     return (
         <div id="mainPageDiv">
 
+            {/*<div><p>{posts === null ? 'loading' : posts}</p></div>*/}
+            <div>
+                {posts.map(posts => <div>{posts}</div>)}
+            </div>
             {/*<div>*/}
-            {/*    {posts.map((item, key) =>*/}
-            {/*        (*/}
-            {/*            <tr key={key}>*/}
-            {/*                <td>{item.author}</td>*/}
-            {/*                <td>{item.content}</td>*/}
-            {/*                <td><img style={{width: '50px', height: '50px'}} src={item.image} alt={key}/></td>*/}
-            {/*            </tr>*/}
-            {/*        ))}*/}
+            {/*    <p>Hello</p>*/}
+            {/*    <p>{posts2}</p>*/}
+            {/*    /!*{posts2.map((item, key) =>*!/*/}
+            {/*    /!*    (*!/*/}
+            {/*    /!*        <tr>*!/*/}
+            {/*    /!*            <td>{item.author}</td>*!/*/}
+            {/*    /!*            <td>{item.content}</td>*!/*/}
+            {/*    /!*        </tr>*!/*/}
+            {/*    /!*    ))}*!/*/}
             {/*</div>*/}
 
             <ChakraProvider>
-                <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                    <Image src={chirper12} alt={chirper12} />
-                    <Box p="6">
-                        <Box d="flex" alignItems="baseline">
-                            <Badge borderRadius="full" px="2" colorScheme="teal">
-                                New
-                            </Badge>
-                            <Box
-                                color="gray.500"
-                                fontWeight="semibold"
-                                letterSpacing="wide"
-                                fontSize="xs"
-                                textTransform="uppercase"
-                                ml="2"
-                            >
-                                {posts.author} beds &bull; {posts.content} baths
-                            </Box>
-                        </Box>
+                {/*{response.data.map((item, key) =>*/}
+                {/*console.log('mapowanie ' + item.likes))}*/}
 
-                        <Box
-                            mt="1"
-                            fontWeight="semibold"
-                            as="h4"
-                            lineHeight="tight"
-                            isTruncated
-                        >
-                            {posts.author}
-                        </Box>
+                {/*<Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">*/}
+                {/*    <Image src={chirper12} alt={chirper12}/>*/}
+                {/*    <Box p="6">*/}
+                {/*        <Box d="flex" alignItems="baseline">*/}
+                {/*            <Badge borderRadius="full" px="2" colorScheme="teal">*/}
+                {/*                New*/}
+                {/*            </Badge>*/}
+                {/*            <Box*/}
+                {/*                color="gray.500"*/}
+                {/*                fontWeight="semibold"*/}
+                {/*                letterSpacing="wide"*/}
+                {/*                fontSize="xs"*/}
+                {/*                textTransform="uppercase"*/}
+                {/*                ml="2"*/}
+                {/*            >*/}
+                {/*                {posts.author} beds &bull; {posts.content} baths*/}
+                {/*            </Box>*/}
+                {/*        </Box>*/}
 
-                        <Box>
-                            {posts.author}
-                            <Box as="span" color="gray.600" fontSize="sm">
-                                / wk
-                            </Box>
-                        </Box>
+                {/*        <Box*/}
+                {/*            mt="1"*/}
+                {/*            fontWeight="semibold"*/}
+                {/*            as="h4"*/}
+                {/*            lineHeight="tight"*/}
+                {/*            isTruncated*/}
+                {/*        >*/}
+                {/*            {posts.content}*/}
+                {/*        </Box>*/}
 
-                        <Box d="flex" mt="2" alignItems="center">
-                            <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                                {posts.author} reviews
-                            </Box>
-                        </Box>
-                    </Box>
-                </Box>
+                {/*        <Box>*/}
+                {/*            {posts.content}*/}
+                {/*            <Box as="span" color="gray.600" fontSize="sm">*/}
+                {/*                / wk*/}
+                {/*            </Box>*/}
+                {/*        </Box>*/}
+
+                {/*        <Box d="flex" mt="2" alignItems="center">*/}
+                {/*            <Box as="span" ml="2" color="gray.600" fontSize="sm">*/}
+                {/*                {posts.likes} reviews*/}
+                {/*            </Box>*/}
+                {/*        </Box>*/}
+                {/*    </Box>*/}
+                {/*</Box>*/}
             </ChakraProvider>
         </div>
     )
